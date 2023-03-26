04650e2ede6f11d4428d2b56ff31351457d1f815
Alexey Kudelko <flexivanov237@gmail.com>
2023-03-26 10:31:06 +0300

    added model users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_verified_email = models.BooleanField(default=False)
+    rating = models.IntegerField(default=0)
+
