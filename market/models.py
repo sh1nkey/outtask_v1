344be252711ad2b6eaaf5221624e1b46daf76f00
Alexey Kudelko <flexivanov237@gmail.com>
2023-03-29 11:29:35 +0300

    little changes

--- market/models.py
+++ market/models.py
@@ -4,13 +4,13 @@ from users.models import User
 # Create your models here.
 
 
-class ModelSubject (models.Model):
+class ModelSubject(models.Model):
     subj_name = models.CharField(blank=True, max_length=30)
 
     def __str__(self):
         return f'{self.subj_name}'
 
-class ModelUni (models.Model):
+class ModelUni(models.Model):
     uni_name = models.CharField(max_length=60, blank=True)
 
     def __str__(self):
@@ -19,11 +19,12 @@ class ModelUni (models.Model):
 
 
 class ModelOfferOrder (models.Model):
-    subj_id = models.ForeignKey(to=ModelSubject, on_delete=models.CASCADE)
-    uni_id = models.ForeignKey(to=ModelUni, on_delete=models.CASCADE)
-    us_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    subj = models.ForeignKey(to=ModelSubject, on_delete=models.CASCADE)
+    uni = models.ForeignKey(to=ModelUni, on_delete=models.CASCADE)
+    user= models.ForeignKey(to=User, on_delete=models.CASCADE)
     task = models.CharField(max_length=40)
     price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return f'{self.us_id}'
+        return f'{self.user.username}'
+
