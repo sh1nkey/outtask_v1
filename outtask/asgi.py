d79905a065904dc604ea014f85213238921a7724
Alexey Kudelko <flexivanov237@gmail.com>
2023-03-30 19:12:28 +0300

    channels configured

--- outtask/asgi.py
+++ outtask/asgi.py
@@ -6,20 +6,27 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outtask.settings')
 
 application = ProtocolTypeRouter(
     {
         "http" : get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
 
     }
 )
+
 
 
 
