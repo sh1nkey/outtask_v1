717521086f65ec5b34510675049c39b2e6047d0c
Alexey Kudelko <flexivanov237@gmail.com>
2023-04-04 19:54:33 +0300

    new models added

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from chat.models import Room, Messages
+
+
+@admin.register(Room)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['pk']
+
+@admin.register(Messages)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['value']
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
+import datetime
+import uuid
+
 from django.db import models
 
 # Create your models here.
+from users.models import User
+
+
+class Room(models.Model):
+    name = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Messages(models.Model):
+    value = models.CharField(max_length=1000)
+    date = models.DateTimeField(default=datetime.datetime.now, blank=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
