9640e1ef4b182c17531c19ece0cf5c8611e31bad
Alexey Kudelko <flexivanov237@gmail.com>
2023-04-09 08:24:36 +0300

    added & connected css to main page html

--- market/views.py
+++ market/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from market.models import Offer
+from users.models import Uni
 
 
 class MarketListView(ListView):
-    model = Offer
     template_name = 'market/market.html'
     title = 'test'
 
     def get_queryset(self):
-        queryset = super().get_queryset()
-        print(queryset)
-        return queryset
+        offers = Offer.objects.all()
+        unis = Uni.objects.all()
+        unis_check = self.kwargs.get('uni_name')
+        return offers.filter(uni_name=unis_check) if unis_check else offers, unis #допилить фильрацию по ВУЗу
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['offers'] = Offer.objects.all()
+        context['unis'] = Uni.objects.all()
         return context
+
+class IndexView(TemplateView):
+    template_name = 'market/index.html' #
--- outtask/urls.py
+++ outtask/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from market.views import IndexView
+
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('chat/', include('chat.urls')),
